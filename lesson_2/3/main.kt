293f68add1b05f108675aacae8f264b4d2db069d
Kris1100 <bobr.200@yandex.ru>
2022-03-28 21:28:22 +0300

    Homework 2

--- lesson_2/3/main.kt
+++ lesson_2/3/main.kt
@@ -1,22 +1,21 @@
 interface FileSystemNode{
-    var parent : FileSystemNode	
+    var parent : FileSystemNode?
     var name : String
-    fun getParent() : FileSystemNode
-    fun getName() : String
+    fun getParent2() : FileSystemNode?
+    fun getName2() : String
     fun getPath() : String
-    fun setParent(parent : FileSystemNode) : Unit
+    fun setParent2(parent : FileSystemNode?) : Unit
 }
 
 abstract class AbstractFileSystemNode : FileSystemNode{
-    override lateinit var parent : FileSystemNode
-    	
+    override var parent : FileSystemNode? = null
     override abstract var name : String
     
-    override fun getParent() : FileSystemNode{
+    override fun getParent2() : FileSystemNode?{
         return this.parent
     }
 
-    override fun getName() : String{
+    override fun getName2() : String{
         return this.name
     }
     
@@ -25,14 +24,14 @@ abstract class AbstractFileSystemNode : FileSystemNode{
         var curr : FileSystemNode? = this
         while (true){
             if (curr != null){
-            	res = curr.getName() + "\\" + res 
-            	curr = curr.getParent()
+            	res = curr.getName2() + "\\" + res 
+            	curr = curr.getParent2()
             }
             else break
         }
         return res
     }
-     	override fun setParent(parent : FileSystemNode) : Unit{
+     	override fun setParent2(parent : FileSystemNode?) : Unit{
         this.parent = parent
     }
     
@@ -47,6 +46,9 @@ class Folder : AbstractFileSystemNode{
     constructor(name : String, vararg v: FileSystemNode){
         this.name = name
         this.list = v.toList()
+        for (i in v){
+            i.parent = this
+        }
         
     }
 }
